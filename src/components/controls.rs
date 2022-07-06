d3f56ea0a8950dba0651ef1e2244881ac5e5ed36
huzhengyang <huzhengyang@gridsum.com>
2022-07-07 06:59:16 +0800

    devices

--- src/components/controls.rs
+++ src/components/controls.rs
@@ -1,18 +1,20 @@
 use sycamore::prelude::*;
-
+use sycamore_futures::*;
 use crate::AppState;
 
 
 #[component]
 pub fn Controls<G: Html>(ctx: Scope) -> View<G> {
 
-    let app_state = use_context::<AppState>(ctx);
-    let devices = app_state.devices.get().video_devices();
+    // let app_state = use_context::<AppState>(ctx);
+    // let devices = create_memo(ctx, || {
+    //     app_state.devices.get().video_devices().cloned().collect()
+    // });
 
     view! {ctx,
         div(class="absolute bottom-2 p-5 w-full") {
             div(class="flex justfy-center") {
-                div(class="mb-4 xl:w-496") {
+                div(class="xl:w-96") {
                     // button(class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") {
                     //     "Start"
                     // }
@@ -31,21 +33,23 @@ pub fn Controls<G: Html>(ctx: Scope) -> View<G> {
                     ease-in-out
                     m-0
                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none", aria-label="Default select example") {
-                        // option(value="") {
-                        //     "Select"
+                        option(value="") {
+                            "Select"
+                        }
+                        option(value="user") {
+                            "User"
+                        }
+                        option(value="environment") {
+                            "Environment"
+                        }
+                        // Keyed {
+                        //     iterable: devices,
+                        //     view: | ctx, device| view!{ctx, option(value=device.id){
+                        //         (device.label)
+                        //     }},
+                        //     key: | device| device.id.clone(),
                         // }
-                        // option(value="user") {
-                        //     "User"
-                        // }
-                        // option(value="environment") {
-                        //     "Environment"
-                        // }
-                        KeyedList::new(devices).map(|(id, device)| {
-                            option(value=id) {
-                                device.name
-                            }
-                        })
-                    }   
+                    }
                 }
             }
         }
